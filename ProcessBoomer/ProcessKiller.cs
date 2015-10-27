c30c326d287f299d82e727c638c7c43e541b0e2a
Sergio Luis Para <sergio@sergioluis.es>
2015-10-27 10:14:07 +0100

    Added VisualStudio project.

---
+++ ProcessBoomer/ProcessKiller.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace ProcessBoomer
+{
+    internal static class ProcessKiller
+    {
+        internal struct KilledProcess
+        {
+            internal int Id;
+            internal string Name;
+        }
+
+        internal static KilledProcess KillRandomProcess()
+        {
+            InitRandom();
+
+            Process[] processList = Process.GetProcesses();
+            int badLuckProcess = mRand.Next(0, processList.Length - 1);
+
+            try
+            {
+                Process processToKill = processList[badLuckProcess];
+                KilledProcess killedProcessInfo = new KilledProcess
+                {
+                    Id = processToKill.Id,
+                    Name = processToKill.ProcessName
+                };
+
+                processToKill.Kill();
+                processToKill.WaitForExit();
+
+                mRetries = 0;
+
+                return killedProcessInfo;
+            }
+            catch (Win32Exception)
+            {
+                mRetries++;
+
+                if (mRetries == MAX_RETRIES)
+                {
+                    string errorMessage = string.Format(
+                        "Max retries ({0}) reached. Could not kill process.",
+                        MAX_RETRIES);
+                    throw new Exception(errorMessage);
+                }
+
+                // The process was being killed before we tried to.
+                // We don't have permission to kill the selected process.
+                // Let's try again, shall we?
+                return KillRandomProcess();
+            }
+            catch (Exception ex)
+            {
+                string errorMessage = string.Format(
+                    "Something bad happened while trying to kill process: {0}",
+                    ex.Message);
+                throw new Exception(errorMessage);
+            }
+            
+        }
+
+        static void InitRandom()
+        {
+            if (mRand != null)
+                return;
+
+            mRand = new Random(666);
+        }
+
+        static Random mRand;
+        static int mRetries = 0;
+        static readonly int MAX_RETRIES = 3;
+    }
+}
---
+++ ProcessBoomer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using DreamCheeky;
+
+using static ProcessBoomer.ProcessKiller;
+
+namespace ProcessBoomer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var redButton = new BigRedButton())
+            {
+                redButton.ButtonPressed += (sender, eventArgs) =>
+                {
+                    TryKillProcess();
+                };
+
+                Console.ReadLine();
+            }
+        }
+
+        static void TryKillProcess()
+        {
+            try
+            {
+                KillProcess();
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
+        }
+
+        static void KillProcess()
+        {
+            KilledProcess process = ProcessKiller.KillRandomProcess();
+            Console.WriteLine(string.Format(
+                KILLED_PROCESS_INFORMATION, process.Name, process.Id));
+        }
+
+        static readonly string KILLED_PROCESS_INFORMATION =
+            @"Killed ""{0}"" with ID {1}";
+    }
+}
